cff3e7a6617beee2c64604e475c5bd0c020eaf02
Ayahellall <aya.m.helal@gmail.com>
2023-09-07 17:10:24 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#!/bin/bash
-#include <stodio.h>
+
+#include <stdio.h>
 int main (void)
+
 {
-puts("Programming is like building a multilingual puzzle \n ";
-		return (0);
+puts("\"Programming is like building a multilingual puzzle");
+		return(0);
 
 
 
