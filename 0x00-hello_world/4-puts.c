6a53a6d0974d364d59e014c97fa70bf5ff7c599e
Ayahellall <aya.m.helal@gmail.com>
2023-09-10 01:26:33 -0700

    using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
-
 #include <stdio.h>
-/*this puts function to output 
+/*
+ * the main function is mandatory for compile 
+ * this puts function to output 
  * with return zero */
 int main(void)
 
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts(" \"Programming is like building a multilingual puzzle" );
 		return(0);
-
-
-
-
 
 
 }
