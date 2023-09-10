9b94b041b8a5ec614b4d71357a6ffa05ca3bdf99
Ayahellall <aya.m.helal@gmail.com>
2023-09-10 03:17:01 -0700

    using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-/* main function it's main func 
- * return will return zero value
+/**
+ * main - the function for start the programm
+ *
+ * Return:  return zero value
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+
+return (0);
 }
 
