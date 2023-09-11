bed511d1a27e865379dbed44ca7971915e271541
Ayahellall <aya.m.helal@gmail.com>
2023-09-11 07:13:39 -0700

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return: zero value always
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z' ; ch >= 'a' ; ch--)
+{
+putchar(ch);
+
+}
+putchar('\n');
+return (0);
+}
