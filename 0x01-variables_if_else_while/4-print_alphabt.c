a536492f1fd8b41feebe49826fb93d08a33be6f0
Ayahellall <aya.m.helal@gmail.com>
2023-09-11 06:13:03 -0700

    exception of letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return: zero value always
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch < 'e' ; ch++)
+{
+if (ch == 'e' || ch == 'q')
+{
+continue;
+}
+putchar(ch);
+
+}
+putchar('\n');
+return (0);
+}
+
