1adbe799392216a52b7df17dd006fa530f7a6828
Ayahellall <aya.m.helal@gmail.com>
2023-09-11 06:52:53 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return: zero value always
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+putchar(i + '0');
+
+}
+putchar('\n');
+return (0);
+}
