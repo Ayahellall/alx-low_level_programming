4a9e9c4f5d14e20b04c6a2f7d7d236c2e303e5e4
Ayahellall <aya.m.helal@gmail.com>
2023-09-11 04:21:11 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return: zero value always
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+
+}
+return (0);
+}
