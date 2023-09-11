444592416100a5de3597c7c4650d1180702d2f37
Ayahellall <aya.m.helal@gmail.com>
2023-09-11 06:27:13 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return: zero value always
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i <= 10 ; i++)
+{
+printf("%d", i);
+
+}
+
+return (0);
+}
