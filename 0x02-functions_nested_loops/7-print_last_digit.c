6d58140f26b717456f25466513c877a936a5a60e
Ayahellall <aya.m.helal@gmail.com>
2023-09-16 08:31:14 -0700

    hard task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_last_digit - func to print last num
+ * @x: value to be checked
+ * Return: return last digit
+ */
+int print_last_digit(int x)
+{
+int last_digit;
+last_digit = x % 10;
+if (x < 0)
+{
+last_digit = last_digit * -1;
+}
+_putchar(last_digit +'0');
+return (last_digit);
+
+
+}
+
