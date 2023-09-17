93f4a49ab93bc39bcb4caf59de18fafec27fed43
Ayahellall <aya.m.helal@gmail.com>
2023-09-17 07:47:33 -0700

    ascii num

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * _isdigit - func to check the digit
+ * @c: value to be checked
+ * Return: will be 1 if itin the range
+ */
+int _isdigit(int c)
+{
+if ((c >= 48) && (c <= 57))
+return (1);
+else
+return (0);
+
+}
