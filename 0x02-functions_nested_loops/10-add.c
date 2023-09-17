dae119d5ada3c7c414184fa84e025c8ebccb556f
Ayahellall <aya.m.helal@gmail.com>
2023-09-17 02:15:27 -0700

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * add - func to add 2 num
+ * @x: first num
+ * @y: second num
+ * Return: return with adding
+ */
+int add(int x, int y)
+{
+int z;
+z = x + y;
+return (z);
+
+
+
+
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * times_table - func to print times table
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+
+
+
+
+
+
+
+}
