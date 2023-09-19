fe899536f2d19d9cecbb9913d0dd710cc36c94ec
Ayahellall <aya.m.helal@gmail.com>
2023-09-19 05:16:41 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * swap_int - func to make swap by pointers
+ * @a: first value
+ * @b: second value
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+
+
+
+
+
+}
+
