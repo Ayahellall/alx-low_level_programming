58f89353e71e24e1519597b2509fce5dcd63c3d8
Ayahellall <aya.m.helal@gmail.com>
2023-09-20 06:58:41 -0700

    10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * more_numbers - 10 times of same numbers
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i <= 10; i++)
+{
+_putchar('\n');
+for (j = 0; j <= 14; j++)
+{
+_putchar(j + '0');
+}
+}
+_putchar('\n');
+
+
+
+}
