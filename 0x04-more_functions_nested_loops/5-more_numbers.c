608e17555ea9fc97977efe796bf082b29ead8635
Ayahellall <aya.m.helal@gmail.com>
2023-09-21 05:07:59 -0700

    ascii warning

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,12 @@ void more_numbers(void)
 int i, j;
 for (i = 0; i <= 10; i++)
 {
-_putchar('\n');
 for (j = 0; j <= 14; j++)
 {
-_putchar(j + '0');
-}
+if (j >= 10)
+_putchar('1');
+_putchar(j % 10 + '0');
 }
 _putchar('\n');
-
-
-
+}
 }
