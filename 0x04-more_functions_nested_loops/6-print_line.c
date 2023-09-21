bac3f5ff1b190b899d5d62b5fad7c6a4a53ef489
Ayahellall <aya.m.helal@gmail.com>
2023-09-21 05:37:13 -0700

    counter till unknown n

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_line - func to print line
+ * @n: value to be checked
+ * Return: nothing
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+
+
+
+
+}
