f2c46cf7a8970d19c208abcc7974ad169e087075
Ayahellall <aya.m.helal@gmail.com>
2023-10-30 07:53:29 -0700

    count of arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include"main.h"
 #include<stdio.h>
 /**
- * main - main func to print her name
+ * main - main func to print count of arg
  * @argc: count of argument
  * @argv: vector of argument
  * Return: with value
