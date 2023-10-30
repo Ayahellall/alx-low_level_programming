c553da11fc2c9c7c930416328fe4d4a9114650b1
Ayahellall <aya.m.helal@gmail.com>
2023-10-30 07:22:31 -0700

    anothershapeof main

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,13 @@
 #include"main.h"
+#include<stdio.h>
 /**
- *
+ * main - main func to print her name
+ * @argc: count of argument
+ * @argv: vector of argument
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
