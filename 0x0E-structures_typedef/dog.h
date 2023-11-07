fd9a43aff97477422db5e418da48e2f3301bab53
Ayahellall <aya.m.helal@gmail.com>
2023-11-07 03:48:52 -0800

    1st stru

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef POPPY
+#define POPPY
+/**
+ * struct dog - details about dog
+ * @name: first member
+ * @age: second member
+ * @owner: thirs member
+ * Description: details about his name,age,owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+#endif
